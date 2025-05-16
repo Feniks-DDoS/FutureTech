535ad94be766c5de6772cf99085401a7a417894a
Valentyn <valentindikhtyarenko2004@gmail.com>
2025-05-16 14:42:34 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
 import LikeCollection from "./Blog-actions.js";
+import VideoPlayerCollection from "./Video-player.js";
 
 
 new Header()
@@ -8,3 +9,5 @@ new Header()
 new TabsCollection()
 
 new LikeCollection()
+
+new VideoPlayerCollection()
